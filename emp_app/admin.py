912ad2398930f0e4c55f5c0f87e03b7b39a4a8f9
Soni Kumari <76401953+sonikumari231@users.noreply.github.com>
2022-07-06 21:07:17 +0530

    Add files via upload

---
+++ emp_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Emplyee,Role,Department
+
+# Register your models here.
+admin.site.register(Emplyee)
+admin.site.register(Role)
+admin.site.register(Department)
